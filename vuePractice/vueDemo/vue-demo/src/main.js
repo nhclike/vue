2c17d4fd44ed5f896c16e37b8d8a344f86f791d5
nhclike <2471814041@qq.com>
2017-10-14 12:33:40 +0800

    开始实战项目

--- vuePractice/vueDemo/vue-demo/src/main.js
+++ vuePractice/vueDemo/vue-demo/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
-
+//import {sum,minus} from './util'
+import * as util from './util'
+Vue.config.productionTip = false;
+/*console.log(`sum:${sum(2,4)}`);
+console.log(`minus:${minus(2,4)}`);*/
+console.log(`sum:${util.sum(2,4)}`);
+ console.log(`minus:${util.minus(2,4)}`);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ vuePractice/vueDemo/vue-demo/src/util.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lij on 2017/10/12.
+ */
+export let sum=(x,y)=>{
+  return x+y;
+}
+export let minus=(m,n)=>{
+  return m-n;
+}

6af9b37c6d510aae41fbd4e995e5ffd589689727
nhclike <2471814041@qq.com>
2018-05-16 14:24:43 +0800

    增加按照价格排序和分页功能
    1前台axios传参
    2后台express解析前台传来的参数

--- vuePractice/vueProject/vue-project/server/routes/goods.js
+++ vuePractice/vueProject/vue-project/server/routes/goods.js
@@ -18,7 +18,15 @@ mongoose.connection.on("deconnected",function () {
 });
 
 router.get('/',function (req,res,next) {
-  Goods.find({},function (err,doc) {
+  let page=parseInt(req.param("page"));//解析前台传过来的参数；第几页
+  let pageSize=parseInt(req.param("pageSize")); //每页大小
+  let sort=parseInt(req.param("sort")); //排序方式
+  let skip=(page-1)*pageSize; //从第几个数据开始
+  let params={};
+  let goodsModel=Goods.find(params).skip(skip).limit(pageSize);
+  goodsModel.sort({'salePrice':sort});
+
+  goodsModel.exec(function (err,doc) {
     if(err){
       res.json({
         status:'1',
--- vuePractice/vueProject/vue-project/src/views/GoodsList.vue
+++ vuePractice/vueProject/vue-project/src/views/GoodsList.vue
@@ -7,7 +7,7 @@
           <div class="filter-nav">
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
-            <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+            <a href="javascript:void(0)" class="price" @click="sortGoods">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
             <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
@@ -32,7 +32,7 @@
                     </div>
                     <div class="main">
                       <div class="name">{{item.productName}}</div>
-                      <div class="price">{{item.productPrice}}</div>
+                      <div class="price">{{item.salePrice}}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
@@ -82,7 +82,10 @@
               ],
               priceCheck:'all',
               filterBy:false,
-              overLayFlag:false
+              overLayFlag:false,
+              sortFlag:true,
+              page:1,
+              pageSize:8
             }
         },
       components:{
@@ -95,12 +98,25 @@
       },
       methods:{
         getGoodList(){
-          axios.get('/goods').then((result)=>{
+          var param={
+            page:this.page,
+            pageSize:this.pageSize,
+            sort:this.sortFlag? 1: -1
+          };
+          axios.get('/goods',{
+            params:param
+          }).then((result)=>{
            // alert(result);
             var res=result.data;
+            console.log(res);
             this.goodList=res.result.list;
-            console.log(this.goodList+'goodList');
+            //console.log(this.goodList+'goodList');
           })
+        },
+        sortGoods(){
+          this.sortFlag=!this.sortFlag;
+          this.page=1;
+          this.getGoodList()
         },
         showFilterPop(){
           this.filterBy=true;
